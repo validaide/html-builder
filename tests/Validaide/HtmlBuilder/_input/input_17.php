27baed12b91885f34243af0b11a4893b3a561b7e
Mark Bijl <mark.bijl@validaide.com>
2021-02-11 17:38:26 +0100

    - more means to change Title [#176673997]

--- tests/Validaide/HtmlBuilder/_input/input_17.php
+++ tests/Validaide/HtmlBuilder/_input/input_17.php
@@ -5,6 +5,7 @@ use Validaide\HtmlBuilder\HTML;
 //
 return function () {
     return HTML::create(HTML::SPAN)
+        ->text('Hi there')
         ->title('My Title')
         ->titleAppend('With Extra content');
 };
